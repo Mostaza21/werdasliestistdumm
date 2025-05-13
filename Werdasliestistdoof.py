1aecc1bd11fe253a11ab1c1c1678954655dcb2a7
Mostaza21 <aylinh2106@gmail.com>
2025-05-13 14:31:59 +0200

    Update Werdasliestistdoof.py

--- Werdasliestistdoof.py
+++ Werdasliestistdoof.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 X = ["Kai", "Aylin", "Chris", "Danny"]
-Y = ["Dumpfbacke", "Lootgeier", "Schlaubi-Schlumpf", "Noob", "Klugscheißinator", "Alter Sack", "Synapsenschoner", "RAM-Rentner", "Trottel", "taubes Nüsschen", "Mettigel", "fetter Keks", "XP-Schmarotzer", "Bug in der Matrix", "bisschen zu fett"]
+Y = ["Dumpfbacke", "Pummelfee", "Lootgeier", "Schlaubi-Schlumpf", "Noob", "Klugscheißinator", "Alter Sack", "Synapsenschoner", "RAM-Rentner", "Trottel", "taubes Nüsschen", "Mettigel", "fetter Keks", "XP-Schmarotzer", "Bug in der Matrix", "bisschen zu fett"]
 
 st.title ("Wer das liest ist dumm")
 
@@ -14,4 +14,4 @@ if st.button("Klick mich sanft und zärtlich"):
 	I = random.choice(X)
 	II = random.choice(Y)
 
-st.write(f"**{I}** ist ein **{II}**")
+st.write(f"**{I}** ist ein(e) **{II}**")
